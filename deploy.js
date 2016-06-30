6796c2feea38ebd341ffe592f7b23e2c75ce3fda
Kevin Busse <kevin.busse@gmail.com>
2016-06-30 12:02:28 +0200

    Initial commit

---
+++ deploy.js
@@ -0,0 +1,67 @@
+var https = require('https');
+var path = require('path');
+var fs = require('fs');
+
+var config;
+try {
+  config = require('./config.json');
+} catch (err) {
+  console.log(`Could not load config file (${err.message})`);
+  process.exit(1);
+}
+
+if (typeof config.email !== 'string' || config.email.length === 0) {
+  console.log('Email is missing');
+  process.exit(1);
+}
+
+if (typeof config.password !== 'string' || config.password.length === 0) {
+  console.log('Password is missing');
+  process.exit(1);
+}
+
+var branch = config.branch || 'default';
+
+console.log(`Deploying to branch "${branch}"`);
+
+var payload = {
+  branch: branch,
+  modules: {}
+};
+
+function loadFileContent(src) {
+  return fs.readFileSync(path.join(__dirname, src), {encoding: 'utf-8'});
+}
+
+try {
+  payload.modules.main = loadFileContent('src/main.js');
+  fs.readdirSync(path.join(__dirname, 'src/node_modules')).forEach((file) => {
+    payload.modules[path.parse(file).name] = loadFileContent('src/node_modules/' + file);
+  });
+} catch (err) {
+  console.log(err.message);
+  process.exit(1);
+}
+
+var req = https.request({
+  hostname: 'screeps.com',
+  port: 443,
+  path: '/api/user/code',
+  method: 'POST',
+  auth: config.email + ':' + config.password,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+}, (res) => {
+  res.on('data', (data) => {
+    console.log(data.toString());
+  })
+});
+
+console.log(JSON.stringify(payload, null, 2));
+req.write(JSON.stringify(payload));
+req.end();
+
+req.on('error', (e) => {
+  console.error(e);
+});
---
+++ src/main.js
@@ -0,0 +1,58 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+function spawn(role, count) {
+  var existingRoles = _.filter(Game.creeps, (creep) => creep.memory.role === role);
+  if (existingRoles.length >= count) {
+    return;
+  }
+
+  if (role === 'claimer') {
+    Game.spawns.Hearth.createCreep([CLAIM, MOVE], undefined, {role: role});
+    return;
+  }
+
+  if (Game.spawns.Hearth.canCreateCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE]) === OK) {
+    Game.spawns.Hearth.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, {role: role});
+    return;
+  }
+
+  Game.spawns.Hearth.createCreep([WORK, CARRY, MOVE], undefined, {role: role});
+}
+
+exports.loop = function () {
+  // spawn('claimer', 1);
+  spawn('builder', 2);
+  spawn('upgrader', 3);
+  spawn('harvester', 3);
+
+  var tower = Game.getObjectById('TOWER_ID');
+  if (tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+  })
+    ;
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
+
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role === 'harvester') {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role === 'upgrader') {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role === 'builder') {
+      roleBuilder.run(creep);
+    }
+  }
+};
