60e776fa5cb2eee4a893b3e223a9c53af50b23f2
Kevin Busse <kevin.busse@gmail.com>
2016-06-30 16:46:45 +0200

    Apply JS Standard Style to all code

--- deploy.js
+++ deploy.js
@@ -1,52 +1,52 @@
-var https = require('https');
-var path = require('path');
-var fs = require('fs');
+var https = require('https')
+var path = require('path')
+var fs = require('fs')
 
-var config;
+var config
 try {
-  config = require('./config.json');
+  config = require('./config.json')
 } catch (err) {
-  console.log(`Could not load config file (${err.message})`);
-  process.exit(1);
+  console.log(`Could not load config file (${err.message})`)
+  process.exit(1)
 }
 
 /** @var {string} config.email */
 if (typeof config.email !== 'string' || config.email.length === 0) {
-  console.log('Email is missing');
-  process.exit(1);
+  console.log('Email is missing')
+  process.exit(1)
 }
 
 /** @var {string} config.password */
 if (typeof config.password !== 'string' || config.password.length === 0) {
-  console.log('Password is missing');
-  process.exit(1);
+  console.log('Password is missing')
+  process.exit(1)
 }
 
 /** @var {string} config.branch */
-var branch = 'default';
+var branch = 'default'
 if (typeof config.branch === 'string' && config.branch.length !== 0) {
-  branch = config.branch;
+  branch = config.branch
 }
 
-console.log(`Deploying to branch "${branch}"`);
+console.log(`Deploying to branch "${branch}"`)
 
 var payload = {
   branch: branch,
   modules: {}
-};
+}
 
-function loadFileContent(src) {
-  return fs.readFileSync(path.join(__dirname, src), {encoding: 'utf-8'});
+function loadFileContent (src) {
+  return fs.readFileSync(path.join(__dirname, src), {encoding: 'utf-8'})
 }
 
 try {
-  payload.modules.main = loadFileContent('src/main.js');
+  payload.modules.main = loadFileContent('src/main.js')
   fs.readdirSync(path.join(__dirname, 'src/node_modules')).forEach((file) => {
-    payload.modules[path.parse(file).name] = loadFileContent('src/node_modules/' + file);
-  });
+    payload.modules[path.parse(file).name] = loadFileContent('src/node_modules/' + file)
+  })
 } catch (err) {
-  console.log(err.message);
-  process.exit(1);
+  console.log(err.message)
+  process.exit(1)
 }
 
 var req = https.request({
@@ -60,14 +60,14 @@ var req = https.request({
   }
 }, (res) => {
   res.on('data', (data) => {
-    console.log(data.toString());
+    console.log(data.toString())
   })
-});
+})
 
-// console.log(JSON.stringify(payload, null, 2));
-req.write(JSON.stringify(payload));
-req.end();
+// console.log(JSON.stringify(payload, null, 2))
+req.write(JSON.stringify(payload))
+req.end()
 
 req.on('error', (e) => {
-  console.error(e);
-});
+  console.error(e)
+})
--- src/main.js
+++ src/main.js
@@ -1,59 +1,59 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require('role.harvester')
+var roleUpgrader = require('role.upgrader')
+var roleBuilder = require('role.builder')
 
-function spawner(role, count) {
-  var existingRoles = _.filter(Game.creeps, (creep) => creep.memory.role === role);
+function spawner (role, count) {
+  var existingRoles = _.filter(Game.creeps, (creep) => creep.memory.role === role)
   if (existingRoles.length >= count) {
-    return;
+    return
   }
 
-  var spawn = Game.spawns.Hearth;
+  var spawn = Game.spawns.Hearth
 
   if (role === 'claimer') {
-    spawn.createCreep([CLAIM, MOVE], '', {role: role});
-    return;
+    spawn.createCreep([CLAIM, MOVE], '', {role: role})
+    return
   }
 
   if (spawn.canCreateCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE]) === OK) {
-    spawn.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], '', {role: role});
-    return;
+    spawn.createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], '', {role: role})
+    return
   }
 
-  spawn.createCreep([WORK, CARRY, MOVE], '', {role: role});
+  spawn.createCreep([WORK, CARRY, MOVE], '', {role: role})
 }
 
 exports.loop = function () {
-  // spawn('claimer', 1);
-  spawner('builder', 2);
-  spawner('upgrader', 3);
-  spawner('harvester', 2);
+  // spawn('claimer', 1)
+  spawner('builder', 2)
+  spawner('upgrader', 3)
+  spawner('harvester', 2)
 
   // TODO: extract tower logic
-  // var tower = Game.getObjectById('TOWER_ID');
+  // var tower = Game.getObjectById('TOWER_ID')
   // if (tower) {
   //   var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
   //     filter: (structure) => structure.hits < structure.hitsMax
-  //   });
+  //   })
   //   if (closestDamagedStructure) {
-  //     tower.repair(closestDamagedStructure);
+  //     tower.repair(closestDamagedStructure)
   //   }
   //
-  //   var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+  //   var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS)
   //   if (closestHostile) {
-  //     tower.attack(closestHostile);
+  //     tower.attack(closestHostile)
   //   }
   // }
 
   _.each(Game.creeps, (creep) => {
     if (creep.memory.role === 'harvester') {
-      roleHarvester.run(creep);
+      roleHarvester.run(creep)
     }
     if (creep.memory.role === 'upgrader') {
-      roleUpgrader.run(creep);
+      roleUpgrader.run(creep)
     }
     if (creep.memory.role === 'builder') {
-      roleBuilder.run(creep);
+      roleBuilder.run(creep)
     }
-  });
-};
+  })
+}
